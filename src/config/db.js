023dc1cafc0c9a7bc7ccf6b9789b255d840903fb
Aravind Reddy <169029376+Aravind-Reddy07@users.noreply.github.com>
2025-06-03 02:57:53 +0530

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,10 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQL_DATABASE,
 });
 
 export default db;
